75bce7b1724dc1385e8b0f1a4fdbb291e97e7446
Vinicius Santana <santanaviny@gmail.com>
2016-03-10 01:02:55 -0800

    Added express Router and bcryptjs(to encrypt our data,  hash our password) to user.js.  My goal is  to create a user and a user squema.

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,9 @@
-users.js
+// We need to have a user an a user squema
+
+//as a new route express has to know that we are using the express router
+
+var router = require('express').Router();
+
+// Encrypting our data 
+//hash our password
+var bcrypt = require('bcrypt.js');
