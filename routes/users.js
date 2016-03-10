4738365b3ef85f6eb2b24a8d289753edc3aeaa1b
Vinicius Santana <santanaviny@gmail.com>
2016-03-10 01:10:00 -0800

    removing comments from users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,2 @@
-// We need to have a user an a user squema
-
-//as a new route express has to know that we are using the express router
-
 var router = require('express').Router();
-
-// Encrypting our data 
-//hash our password
 var bcrypt = require('bcrypt.js');
