7e219b136ad440403d3bbb3541ff35273fcf811c
Vinicius Santana <santanaviny@gmail.com>
2016-03-10 00:47:57 -0800

    Created module to connect to database

--- config/db.js
+++ config/db.js
@@ -1,1 +1,6 @@
 var db = require('mongoose');
+
+db.connect('mongodb://<dbuser>:<dbpassword>@ds011429.mlab.com:11429/myworkouts');
+
+//making db available for other modules to require db
+module.exports = db;
