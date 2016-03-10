ba8f48dace63bd670781f300e202272157455f33
Vinicius Santana <santanaviny@gmail.com>
2016-03-10 01:50:49 -0800

    Created router.post in users.js and wired up to the server.js

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,28 @@
 var router = require('express').Router();
 var bcrypt = require('bcrypt.js');
-
 var User = require('../models/user');
+
+//res comes from express.router
+router.post('/', (req, res) => {
+	//new user object based on the model
+	// user = {username: 'foo', email: 'blah@cheese.com', pwd: 'blah'}
+	var user = new User({
+		username: req.body.user.username,
+		email: req.body.user.email,
+		passhash: bcrypt, hashSync(req.body.user.pwd, 10)
+	});
+
+	user.save().then(
+		(newuser) => {
+			res.json({
+				user:newuser,
+				message: 'sucess'
+			})
+		},
+		(err) => {
+			res.send(500, err.message);
+		}
+	)
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,14 @@ var app = express();
 var bodyParser = require('body-parser');
 
 
+app.use(bodyParser.json());
+
 app.use('/test', function(req, res){
 	res.send('hello world');
 });
+
+app.use('/api/users', require('.routes/users')); 
+
 
 app.listen(3000,function(){
 	console.log('app is listening on port 3000...');
