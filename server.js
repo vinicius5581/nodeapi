f9e4a563850d5a9e752dd39487983e49173071a9
Vinicius Santana <santanaviny@gmail.com>
2016-03-10 00:22:20 -0800

    Installed express & created test route

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var db = require('mongoose');
+
+app.use('/test', function(req, res){
+	res.send('hello world');
+});
+
+app.listen(3000,function(){
+	console.log('app is listening on port 3000...');
+});
